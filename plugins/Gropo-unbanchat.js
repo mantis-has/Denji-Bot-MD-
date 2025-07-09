2272ff92826cd24f9fe5ff149b44bf6cef51b506
mantis-has <mantisbotmd@gmail.com>
2025-07-09 07:23:59 -0400

    Actualizar Gropo-unbanchat.js

--- plugins/Gropo-unbanchat.js
+++ plugins/Gropo-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '🩵 La bot ya podra responder aquí.', m, rcanal)
+    await conn.reply(m.chat, '🔥 El bot ya podra responder aquí.', m, rcanal)
     await m.react('✅')
 }
 handler.help = ['desbanearbot']
