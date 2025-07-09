0289be813f2301c2417e1921dbc2fd9a80b3a2df
mantis-has <mantisbotmd@gmail.com>
2025-07-09 07:52:49 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -10,10 +10,10 @@ try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`🩵 *¡El video no puede durar mas de 8 segundos!*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`⚡ *¡El video no puede durar mas de 8 segundos!*`)
 let img = await q.download?.()
 
-if (!img) return conn.reply(m.chat, `🩵  *_Responde a un vídeo, imagen o Gif para generar tu sticker._*`, m, rcanal)
+if (!img) return conn.reply(m.chat, `🔥  *_Responde a un vídeo, imagen o Gif para generar tu sticker._*`, m, rcanal)
 
 let out
 try {
@@ -31,16 +31,16 @@ stiker = await sticker(false, out, global.packsticker, global.author)
 } else if (args[0]) {
 if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packsticker, global.author)
 
-else return m.reply(`🩵 El url es incorrecto`)
+else return m.reply(`🔥 El url es incorrecto`)
 
 }
 } catch (e) {
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `🩵 ̸̷᮫໊᷐͢᷍ᰍ STICKERS`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `⁖ฺ۟̇࣪·֗٬̤⃟🔥𝐃𝐞𝐧𝐣𝐢 ✰ Stick ⬣`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
 
-else return conn.reply(m.chat, '🩵 *_Debes responder a un Video, Foto o Gif, para generar su sticker._*', m, rcanal)
+else return conn.reply(m.chat, '🔥 *_Debes responder a un Video, Foto o Gif, para generar su sticker._*', m, rcanal)
 
 
 }}
