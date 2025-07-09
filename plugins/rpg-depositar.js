1cb11a63b151817f6ca1cedc48eda1463f9c9708
mantis-has <mantisbotmd@gmail.com>
2025-07-09 07:51:37 -0400

    Actualizar rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -2,8 +2,8 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🩵 Ingresa la cantidad de *Diamantes* que deseas Depositar.')
-if ((args[0]) < 1) return m.reply('🩵 Ingresa una cantidad válida de *Diamantes*.')
+if (!args[0]) return m.reply('⚡ Ingresa la cantidad de *Diamantes* que deseas Depositar.')
+if ((args[0]) < 1) return m.reply('⚡ Ingresa una cantidad válida de *Diamantes*.')
 if (args[0] == 'all') {
 let count = parseInt(user.monedas)
 user.monedas -= count * 1
@@ -11,7 +11,7 @@ user.bank += count * 1
 await m.reply(`Depositaste *${count} Diamantes* al Banco.`)
 return !0
 }
-if (!Number(args[0])) return m.reply('🩵 La cantidad deve ser un Numero.')
+if (!Number(args[0])) return m.reply('⚡ La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
 if (!user.monedas) return m.reply('No tienes *Diamantes* en la Cartera.')
 if (user.monedas < count) return m.reply(`Solo tienes *${user.monedas} Monedas 🪙* en la Cartera.`)
