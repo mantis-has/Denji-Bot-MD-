6bae7c1ab6d2b997355e46ff79243f05f74dfb26
mantis-has <mantisbotmd@gmail.com>
2025-07-09 07:24:29 -0400

    Actualizar Game•zodiac.js

--- plugins/Game•zodiac.js
+++ plugins/Game•zodiac.js
@@ -41,7 +41,7 @@ let handler = async (m, { conn, text }) => {
         text = m.quoted.text
     } else if (!text) {
         return conn.sendMessage(m.chat, {
-            text: `「🩵」 Responde a un mensaje o ingresa un texto para crear el Sticker estilo blanco.`,
+            text: `「🔥」 Responde a un mensaje o ingresa un texto para crear el Sticker estilo blanco.`,
         }, { quoted: m })
     }
 
@@ -57,11 +57,11 @@ let handler = async (m, { conn, text }) => {
         if (stiker) {
             return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
         } else {
-            throw new Error("🩵 No se pudo generar el sticker.")
+            throw new Error("🔥 No se pudo generar el sticker.")
         }
     } catch (error) {
         return conn.sendMessage(m.chat, {
-            text: `🩵 Ocurrió un error: ${error.message}`,
+            text: `🔥 Ocurrió un error: ${error.message}`,
         }, { quoted: m })
     }
 }
