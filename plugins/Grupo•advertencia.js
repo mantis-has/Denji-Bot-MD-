e45f1d6e593311c80b327b4b097b12d3a0de9523
mantis-has <mantisbotmd@gmail.com>
2025-07-09 07:25:45 -0400

    Actualizar Grupo•advertencia.js

--- plugins/Grupo•advertencia.js
+++ plugins/Grupo•advertencia.js
@@ -11,7 +11,7 @@ who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
   const dReason = 'Sin motivo';
   const msgtext = text || dReason 
   const sdms = msgtext.replace(/@\d+-?\d* /g, '');
-  const warntext = `*💎 Etiquete a una persona o responda a un mensaje del grupo para advertir al usuario*\n\n*Ejemplo:*\n*${usedPrefix + command} @tag*`;
+  const warntext = `*🔥 Etiquete a una persona o responda a un mensaje del grupo para advertir al usuario*\n\n*Ejemplo:*\n*${usedPrefix + command} @tag*`;
   if (!who) {
 return m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext) });
   }
